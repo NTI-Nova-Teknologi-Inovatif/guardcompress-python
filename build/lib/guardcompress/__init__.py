20596f7927889094f71f2c17c96ac1805f8e19a7
GuardCompress <dev@guardcompress.local>
2026-09-09 16:25:38 +0700

    bersihkan: sampah build python jangan ikut repo

--- build/lib/guardcompress/__init__.py
+++
@@ -1,7 +0,0 @@
-from .batch import batch
-from .client import cleanup, process
-from .errors import BlockedError, BusyError
-from .presets import audio, image, video
-
-__all__ = ["process", "image", "video", "audio", "batch", "cleanup",
-           "BlockedError", "BusyError"]
